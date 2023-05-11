a3bb85a4d3b2c0096e2acc17f46b38aca3b38ea6
Calos01 <ca_los01@hotmail.com>
2023-05-11 08:11:11 -0500

    avance crear invoice

--- src/app/createinvoice/createinvoice.component.ts
+++ src/app/createinvoice/createinvoice.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-createinvoice',
@@ -7,24 +7,45 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./createinvoice.component.css']
 })
 export class CreateinvoiceComponent{
-  /**
-   *
-   */
+  
+  pagetitle='Crear Invoice'
+  datadetails!:FormArray<any>
+
   constructor(private builder:FormBuilder) {
     
   }
+  // jalara los datos que envia el formulario
   invoiceform=this.builder.group({
     invoiceno:this.builder.control('', Validators.required),
     customerId:this.builder.control('', Validators.required),
     customerName:this.builder.control(''),
     deliveryAddress:this.builder.control(''),
     remarks:this.builder.control(''),
-    total:this.builder.control(''),
+    total:this.builder.control({value:0, disabled:true}),
     tax:this.builder.control(''),
-    netTotal:this.builder.control('')
+    netTotal:this.builder.control({value:0, disabled:true}),
+    details:this.builder.array([])//array
   });
-
+  //funcion boton save
   SaveInvoice(){
-    
+    console.log(this.invoiceform.value);
+  }
+  //funcion boton add
+  //obtener details del invoiceform y almacenar en datadetails
+  //pushear lo obtenido en el generateRow
+  AddProduct(){
+    this.datadetails=this.invoiceform.get("details") as FormArray;
+    this.datadetails.push(this.GenerateRow());
+  }
+
+  GenerateRow(){
+    return this.builder.group({
+      invoiceno:this.builder.control(''),
+      productCode:this.builder.control('', Validators.required),
+      productName:this.builder.control(''),
+      qty:this.builder.control(1),
+      salesPrice:this.builder.control(0),
+      total:this.builder.control({value:0, disabled:true}),
+    });
   }
 }
