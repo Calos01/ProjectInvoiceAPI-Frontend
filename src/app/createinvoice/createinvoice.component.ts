573528b6ff81a409843ec35c1346e9270431d2fc
Calos01 <ca_los01@hotmail.com>
2023-05-12 09:08:56 -0500

    lista products- selects and event changes

--- src/app/createinvoice/createinvoice.component.ts
+++ src/app/createinvoice/createinvoice.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
-import { FormArray, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MasterService } from '../services/master.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-createinvoice',
   templateUrl: './createinvoice.component.html',
   styleUrls: ['./createinvoice.component.css']
 })
-export class CreateinvoiceComponent{
+export class CreateinvoiceComponent implements OnInit{
   
   pagetitle='Crear Invoice'
   datadetails!:FormArray<any>
 
-  constructor(private builder:FormBuilder) {
+  mastercustomers:any;
+  masterproducts:any;
+
+  productlist!:FormGroup<any>
+
+  constructor(private builder:FormBuilder, private _service:MasterService, private router:Router) {
     
+  }
+  ngOnInit(): void {
+    this.ObtenerCustomers();
+    this.ObtenerProducts();
   }
   // jalara los datos que envia el formulario
   invoiceform=this.builder.group({
@@ -30,7 +41,7 @@ export class CreateinvoiceComponent{
   SaveInvoice(){
     console.log(this.invoiceform.value);
   }
-  //funcion boton add
+  //funcion boton add tABLA pRODUCTS
   //obtener details del invoiceform y almacenar en datadetails
   //pushear lo obtenido en el generateRow
   AddProduct(){
@@ -51,5 +62,42 @@ export class CreateinvoiceComponent{
       salesPrice:this.builder.control(0),
       total:this.builder.control({value:0, disabled:true}),
     });
+  }
+
+  // PARA LISTA DESPLEGABLES SELECTS
+  ObtenerCustomers(){
+    this._service.GetCustomers().subscribe(dat=>{
+      this.mastercustomers=dat;    })
+  }
+  ObtenerProducts(){
+    this._service.GetProducts().subscribe(dat=>{
+      this.masterproducts=dat;    })
+  }
+
+  //Eventos Changes Lista desplegables trae los datos de API
+  changecustomer(){
+    let codecustomer=this.invoiceform.get("customerId")?.value;
+    this._service.GetCustomerById(codecustomer).subscribe(data=>{
+      let customerid:any;
+      customerid=data;
+      if(customerid!=null){
+        this.invoiceform.get("deliveryAddress")?.setValue(customerid.address+","+customerid.phoneno+","+customerid.email)
+      }
+    }
+    )
+  }
+  changeproduct(index:any){
+    let productdetail=this.invoiceform.get("details") as FormArray;
+    this.productlist=productdetail.at(index) as FormGroup;//trae del array detail del invoiceform, la posicion(index) y lo pasa en un formGroup
+    let productCode=this.productlist.get("productCode")?.value;//de ese array obtenemo el productCode para jalar de la api de products
+    this._service.GetProductsById(productCode).subscribe(data=>{//
+      let prodid:any;
+      console.log(data);
+      prodid=data;
+      if(prodid!=null){
+        this.productlist.get("productName")?.setValue(prodid.name);
+        this.productlist.get("salesPrice")?.setValue(prodid.price);
+      }
+    })
   }
 }
--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,18 @@ import { Injectable } from '@angular/core';
 })
 export class MasterService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  GetCustomers(){
+    return this.http.get('https://localhost:7155/Customer')
+  }
+  GetCustomerById(code:any){
+    return this.http.get('https://localhost:7155/Customer/GetCustomer?code='+code)
+  }
+  GetProducts(){
+    return this.http.get('https://localhost:7155/GetProducts')
+  }
+  GetProductsById(code:any){
+    return this.http.get('https://localhost:7155/GetProductId?code='+code)
+  }
 }
